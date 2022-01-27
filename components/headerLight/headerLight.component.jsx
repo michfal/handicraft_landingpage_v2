83da2e56109a6ac1b2b77b3763edee72be1296db
Michał Fal <michfal86@gmail.com>
2022-01-27 16:16:38 +0100

    test

--- components/headerLight/headerLight.component.jsx
+++ components/headerLight/headerLight.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from './headerLight.styles';
 
-export const HeaderLignt = ({children}) => {
+export const HeaderLight = ({children}) => {
     return (
         <Header>{children}</Header>
     )

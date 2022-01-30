9a3fa3db0c5d1a369e3cc578aeda247b16a1b635
Michał Fal <michfal86@gmail.com>
2022-01-30 17:23:48 +0100

    removed import font face from global styles

--- globals/reset.styles.jsx
+++ globals/reset.styles.jsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+/* @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap'); */
 
 html,
 body {
